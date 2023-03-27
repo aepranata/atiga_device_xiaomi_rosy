2958c9a12de35442e8b929579e6f529ae0d4e0d7
Aditya Eka Pranata <aep.repository@gmail.com>
2023-03-27 15:17:44 +0700

    rosy: Welcome LineageOS
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/banana_rosy.mk
+    $(LOCAL_DIR)/lineage_rosy.mk
 
 COMMON_LUNCH_CHOICES := \
-    banana_rosy-user \
-    banana_rosy-userdebug \
-    banana_rosy-eng
+    lineage_rosy-user \
+    lineage_rosy-userdebug \
+    lineage_rosy-eng
--- lineage_rosy.mk
+++ lineage_rosy.mk
@@ -9,25 +9,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common BananaDroid stuff
-$(call inherit-product, vendor/banana/config/common.mk)
-
-BANANA_MAINTAINER := aepranata
-TARGET_FACE_UNLOCK_SUPPORTED := true
+# Inherit some common LineageOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from rosy device
 $(call inherit-product, device/xiaomi/rosy/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := banana_rosy
+PRODUCT_NAME := lineage_rosy
 PRODUCT_DEVICE := rosy
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5
 PRODUCT_MANUFACTURER := Xiaomi
 
 # Boot animation
+TARGET_BOOTANIMATION_NAME := 720
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_BOOTANIMATION_MULTITHREAD_DECODE := true
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 # GApps
 TARGET_GAPPS_ARCH := arm64
