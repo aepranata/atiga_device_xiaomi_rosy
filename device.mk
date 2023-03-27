e6880e71dd93938f1bf1c2e0722af490a1b09ba6
Aditya Eka Pranata <aep.repository@gmail.com>
2023-03-27 15:27:55 +0700

    rosy: Build XiaomiDoze
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- device.mk
+++ device.mk
@@ -181,6 +181,10 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.0.vendor \
     vendor.display.config@2.0
 
+# Doze mode
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
